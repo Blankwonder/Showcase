e87efaa191dcb06bf044a104c9e5c4ddb4a795e1
Blankwonder <l@yach.me>
2022-05-23 16:36:07 +0800

    first commit

--- Showcase/ViewController.m
+++ Showcase/ViewController.m
@@ -7,7 +7,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate> {
+    IBOutlet UIImageView *_imageView;
+}
 
 @end
 
@@ -15,8 +18,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    UIImage *savedImage = [UIImage imageWithContentsOfFile:self.imagePath];
+    
+    if (savedImage) {
+        if (savedImage.size.width > savedImage.size.height) {
+            savedImage = [[UIImage alloc] initWithCGImage:savedImage.CGImage
+                                                    scale:savedImage.scale
+                                              orientation:UIImageOrientationRight];
+        }
+        
+        _imageView.image = savedImage;
+    }
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    if (!_imageView.image) {
+        [self selectImage];
+    }
+}
+
+- (IBAction)selectImage {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+
+    picker.delegate = self;
+    [self presentViewController:picker animated:YES completion:^{}];
+}
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey, id> *)info {
+
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    
+    if (image.size.width > image.size.height) {
+        image = [[UIImage alloc] initWithCGImage:image.CGImage
+                                           scale:image.scale
+                                     orientation:UIImageOrientationRight];
+    }
+    
+    _imageView.image = image;
+    [UIImagePNGRepresentation(image) writeToFile:self.imagePath atomically:YES];
+    
+    [self dismissViewControllerAnimated:YES completion:^{}];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:^{}];
+}
+
+- (NSString *)imagePath {
+    return [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES).firstObject stringByAppendingPathComponent:@"image"];
+}
 
 @end
